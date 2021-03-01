8167ef1d1cb4482af45616bb8e3795708d049f5b
Chris Hayes <christophayes@gmail.com>
2021-03-01 02:59:26 -0500

    Add poly planet and moon; Simple rotate + orbit scripts

---
+++ ThePanphinConspiracyTakeTwo/Assets/orbitPlanet.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class orbitPlanet : MonoBehaviour
+{
+    public Transform target;
+    public float orbitDistance = 10.0f;
+    public float orbitDegressPerSec = 180.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Orbit()
+    {
+        if (target != null)
+        {
+            // Keep at orbitDistance from target
+            transform.position = target.position + (transform.position - target.position).normalized * orbitDistance;
+            transform.RotateAround(target.position, Vector3.up, orbitDegressPerSec * Time.deltaTime);
+        }
+    }
+
+    // Be sure target is done moving before updating this object's position
+    void LateUpdate()
+    {
+        Orbit();
+    }
+}
