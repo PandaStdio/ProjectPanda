4af108ab30d833f35caf2923016ec95101d1a930
Chris <christophayes@gmail.com>
2021-03-23 21:05:49 -0400

    Messing around

---
+++ Assets/Scripts/LaunchOrbit.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SimpleKeplerOrbits;
+
+public class LaunchOrbit : MonoBehaviour
+{
+    public SimpleKeplerOrbits.Vector3d bodyPosition;
+    public SimpleKeplerOrbits.Vector3d bodyVelocity;
+    public float attractorMass;
+    public float gConst;
+    public Transform attractorTransform;
+    private SimpleKeplerOrbits.KeplerOrbitMover body;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<KeplerOrbitMover>();
+        body.AttractorSettings.AttractorObject = attractorTransform;
+        body.AttractorSettings.AttractorMass = attractorMass;
+        body.AttractorSettings.GravityConstant = gConst;
+        body.OrbitData = new KeplerOrbitData(
+            position: bodyPosition,
+            velocity: bodyVelocity,
+            attractorMass: attractorMass,
+            gConst: gConst);
+        body.ForceUpdateViewFromInternalState();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        body.ForceUpdateViewFromInternalState();
+    }
+}
